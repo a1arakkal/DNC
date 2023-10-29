02005639de5e7b257ac4bf0472c842b973f21e0f
a1arakkal <alan-arakkal@uiowa.edu>
2023-10-29 15:46:43 -0500

    updates

--- R/rcpp_funs.R
+++ R/rcpp_funs.R
@@ -7,9 +7,9 @@
 #' @useDynLib DNC   
 #' @export
 chol_inv <- function(x) {
-  .Call(`_DNC_chol_inv`, x, PACKAGE = "DNC")
+  .Call('_DNC_chol_inv', PACKAGE = 'DNC', x)
+  
 }
-
 #' log_post_fun - a function to compute the log posterior
 #' @return A scalar 
 #' @details
@@ -33,4 +33,3 @@ log_post_fun <- function(params, x, y, mu, sigma) {
 log_post_fun_dnc <- function(params, x, y, mu, sigma, ratio) {
   .Call('_DNC_log_post_fun_dnc', PACKAGE = 'DNC', params, x, y, mu, sigma, ratio)
 }
-
